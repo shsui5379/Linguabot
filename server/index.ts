277a72bd27d5eb1a709cc63f8b55ce37bb29fbac
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-03-05 03:06:38 -0500

    base

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+const app = express();
+
+// This code makes sure that any request that does not matches a static file
+// in the build folder, will just serve index.html. Client side routing is
+// going to make sure that the correct content will be loaded.
+app.use((req, res, next) => {
+    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+        next();
+    } else {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    }
+});
+app.use(express.static(path.join(__dirname, '../client/build')));
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
