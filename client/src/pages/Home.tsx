af9f1e48f509898f894c1fdc8196b63b65b78a2a
tkanie123 <tk.anie123@gmail.com>
2024-03-09 19:12:45 -0500

    Added color and feature highlight template

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,12 +4,14 @@ export default function Home() {
   return (
     <>
       <div id="welcome-block">
-        <div id="welcome-inner-text">
-          <h1 className="welcome-title" id="upper-title">CHATBOT</h1>
-          <h2 className="welcome-title" id="lower-title">language partner</h2>
-          <p id="welcome-subtitle">Improve your language fluency by chatting with an AI partner!</p>
-        </div>
-        <img id="welcome-image"></img> 
+        <div id="welcome-inner-block">
+          <div id="welcome-inner-text">
+            <h1 className="welcome-title" id="upper-title">CHATBOT</h1>
+            <h2 className="welcome-title" id="lower-title">language partner</h2>
+            <p id="welcome-subtitle">Improve your language fluency by chatting with an AI partner!</p>
+          </div>
+          <img id="welcome-image" src="https://kioku-space.com/images/chatbot.webp"></img> 
+      </div>
       </div>
       <div id="major-features">
         <h3 id="problem">Problem Statement</h3>
@@ -29,6 +31,26 @@ export default function Home() {
             <i></i>
             <p className="feature-title">Feature</p>
             <p className="feature-description">Feature description</p>
+          </div>
+        </div>
+      </div>
+      <div id="feature-display">
+        <div className="feature-highlight-overview image-left">
+          <div className="feature-highlight">
+            <p className="feature-highlight-title">Feature 1</p>
+            <p className="feature-highlight-description">Feature description in detail</p>
+          </div>
+        </div>
+        <div className="feature-highlight image-right">
+          <div className="feature-highlight">
+            <p className="feature-highlight-title">Feature 2</p>
+            <p className="feature-highlight-description">Feature description in detail</p>
+          </div>
+        </div>
+        <div className="feature-highlight image-left">
+          <div className="feature-highlight">
+            <p className="feature-highlight-title">Feature 3</p>
+            <p className="feature-highlight-description">Feature description in detail</p>
           </div>
         </div>
       </div>
