8401cc26d84cee8c4584c01a811c9f1c08976584
tkanie123 <tk.anie123@gmail.com>
2024-03-12 22:14:46 -0400

    Language selection page before onclicks

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,4 +1,25 @@
 // Sign up component
+import "../css/SignUp.css"
+
 export default function SignUp() {
-  return <h1>this is the sign up page</h1>;
+  const languages_supported = ["English", "Spanish", "French", "Mandarin", "Japanese", "Korean"];
+  var rendered_languages = [languages_supported.map(item => 
+    <button className="lang-unique-button" id={item} style={{backgroundImage:`url("")`}}>
+      <img className="lang-unique-flag" 
+           id={item + '-flag'}
+           src={require("/src/assets/flags/" + item +".png")}
+           alt={item}>
+      </img>
+      <div className="lang-unique-text">{item}</div>
+    </button>
+  )];
+  return(
+    <>
+      <p id="lang-select-instruction-title">Please select your target language.</p>
+      <p id="lang-select-instruction-subtitle">Linguabot will communicate with you in this language!</p>
+      
+      <div id="lang-select">{rendered_languages}</div>
+      <button id="continue-lang-select">CONTINUE</button>
+    </>
+  );
 }
