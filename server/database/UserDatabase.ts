b5b4a47dd56e156d031b3825999aacc1cd71315a
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-03-26 01:35:33 -0400

    more sanity checks and throws

--- server/database/UserDatabase.ts
+++ server/database/UserDatabase.ts
@@ -71,6 +71,11 @@ async function fetchUser(userId: string): Promise<User | null> {
  * @returns Array where index 0 is the user that was described by the parameters. Index 1 is whether the user is just newly created in the database.
  */
 async function createUser(userId: string, firstName: string, lastName: string, sourceLanguage: Language, targetLanguages: Language[]): Promise<[User, boolean] | null> {
+    if (userId.length === 0) throw new Error("User Id cannot have 0 length");
+    if (firstName.length === 0 || firstName.length > 255) throw new Error("First name length must be between 0 and 255 characters long");
+    if (lastName.length === 0 || lastName.length > 255) throw new Error("Last name length must be between 0 and 255 characters long");
+    if (targetLanguages.length === 0) throw new Error("User must have at least one target language");
+
     try {
         const [instance, justCreated] = await UserDatabase.findOrCreate({
             where: { userId: userId },
