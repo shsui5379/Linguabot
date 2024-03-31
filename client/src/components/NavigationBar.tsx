ac5823c386714d21c13b5e21fbbcbd69b332a61f
Kevin Feng <kevinfbths@gmail.com>
2024-03-30 23:25:55 -0400

    fixed logout login

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -2,41 +2,17 @@ import React, { useState, useEffect  } from 'react';
 import { Link } from "react-router-dom";
 import "../css/NavigationBar.css";   
 import LoginButton from './LoginButton'; 
-import SignUpButton from './SignUpButton'
+import SignUpButton from './SignUpButton'; 
+import LogoutButton from './LogoutButton'; 
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Navigation bar component
 export default function NavigationBar() {
   // Determine the correct things to display on the right side of the navigation bar
+  const { user, isAuthenticated, isLoading } = useAuth0();
   let navRight = null; 
-
-  const handleLogout = () => {
-    window.location.href = '/logout'; 
-  }
- 
-  // Set login status to false initally 
-  const [isLoggedIn, setLoggedIn] = useState(false);
-
-  // Set logged in status after callback from login
-  useEffect(() => { 
-    async function fetchLoginStatus() {
-      try {
-        const response = await fetch('/status');
-        const data =  await response.text(); 
-        if (data === 'Logged in') {
-          setLoggedIn(true); 
-        } else {
-          setLoggedIn(false);
-        }
-      } catch (error) {
-        console.error('Error fetching login status:', error);
-        setLoggedIn(false); // Default to false if there's an error
-      }
-    } 
-    fetchLoginStatus();
-  }, []); 
-
   
-  if (!isLoggedIn) {
+  if (!isAuthenticated) {
     navRight = (
       <>
           <SignUpButton/>
@@ -46,9 +22,7 @@ export default function NavigationBar() {
   } else {
     navRight = (
       <>
-        <Link className="navlink" id="navlink-rightmost"  to="/">
-          <p className="nav-button-filled" onClick={handleLogout}> LOG OUT </p>
-        </Link>
+        <LogoutButton/>
         <Link className="navlink" to="/chat">
           <p className="nav-button-white"> CHAT </p>
         </Link>
@@ -61,7 +35,7 @@ export default function NavigationBar() {
       <Link id="logo" to="/">
         Linguabot
       </Link>
-      {navRight}
+      {navRight} 
     </div>
   );
 }
