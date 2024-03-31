86839655405b04fd3e2c72baac550cbe548c97cf
Kevin Feng <kevinfbths@gmail.com>
2024-03-30 20:13:42 -0400

    fixed signup

---
+++ client/src/components/SignUpButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect({
+    authorizationParams: {
+        screen_hint: 'signup'
+    }
+  })}> Sign Up</button>;
+};
+
+export default LoginButton;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react'; 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+<Auth0Provider
+    domain="dev-3pimm2jcsp5tvdbf.us.auth0.com"
+    clientId="1d3BbX1WLyPSi1m6Uw01iWNOMKEbBuVL"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
