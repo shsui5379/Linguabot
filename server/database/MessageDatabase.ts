520930581e1de9bfc6a57bd6ce1c63a3ddd5d3e5
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-04-08 00:13:17 -0400

    chat and message sequelize models

---
+++ server/database/MessageDatabase.ts
@@ -0,0 +1,31 @@
+import Sequelize from "sequelize";
+
+class MessageDatabase extends Sequelize.Model { };
+
+function init(sequelize: Sequelize.Sequelize) {
+    MessageDatabase.init({
+        messageId: {
+            type: Sequelize.DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true
+        },
+        chatId: {
+            type: Sequelize.DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: Sequelize.DataTypes.STRING,
+            allowNull: false
+        },
+        note: {
+            type: Sequelize.DataTypes.STRING
+        },
+        starred: {
+            type: Sequelize.DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    }, { sequelize });
+}
+
+export default { init, MessageDatabase }
