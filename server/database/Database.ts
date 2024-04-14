a5ddf5d818190257f4a761cbdb434277d89879f0
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-04-13 23:43:02 -0400

    bring back select sequelize native relationships

--- server/database/Database.ts
+++ server/database/Database.ts
@@ -10,6 +10,12 @@ ChatDatabase.init(sequelize);
 MessageDatabase.init(sequelize);
 UserDatabase.init(sequelize);
 
+UserDatabase.UserDatabase.hasMany(ChatDatabase.ChatDatabase, { foreignKey: "userId" });
+ChatDatabase.ChatDatabase.belongsTo(UserDatabase.UserDatabase, { foreignKey: "userId" });
+
+ChatDatabase.ChatDatabase.hasMany(MessageDatabase.MessageDatabase, { foreignKey: "chatId" });
+MessageDatabase.MessageDatabase.belongsTo(ChatDatabase.ChatDatabase, { foreignKey: "chatId" });
+
 async function initialize() {
     await sequelize.authenticate().catch((error) => { console.error("Error connecting to database: ", error) });
     await sequelize.sync({ alter: true }).catch((error) => { console.error("Error syncing database model: ", error) });
