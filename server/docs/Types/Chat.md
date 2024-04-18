46a29a251397fa84f2305106658b8a13e59db89b
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-04-17 21:05:02 -0400

    remove some reasonably unneeded mutators

--- server/docs/Types/Chat.md
+++ server/docs/Types/Chat.md
@@ -7,13 +7,10 @@ Class representing an instance of a Chat
 - databaseRecord: ChatDatabase sequelize construct
   - privately stored reference to the database record
 - chatId: string
-  - via public setters/getters
   - pulled from record
 - userId: string
-  - via public setters/getters
   - pulled from record
 - language: string
-  - via public setters/getters
   - pulled from record
 
 ## Methods
--- server/types/Chat.ts
+++ server/types/Chat.ts
@@ -11,29 +11,12 @@ class Chat {
         return this.#databaseRecord.chatId;
     }
 
-    setChatId(newId: string) {
-        if (newId.length === 0) throw new Error("Chat ID cannot be empty");
-        this.#databaseRecord.chatId = newId;
-        this.#databaseRecord.save().catch((e: Error) => { console.error("Error saving chatId for chat ", this.#databaseRecord.chatId, e) });
-    }
-
     get userId(): string {
         return this.#databaseRecord.userId;
     }
 
-    setUserId(newId: string) {
-        if (newId.length === 0) throw new Error("User ID cannot be empty");
-        this.#databaseRecord.userId = newId;
-        this.#databaseRecord.save().catch((e: Error) => { console.error("Error saving userId for chat ", this.#databaseRecord.chatId, e) });
-    }
-
     get language(): Language {
         return this.#databaseRecord.chatId;
-    }
-
-    setLanguage(newLanguage: Language) {
-        this.#databaseRecord.language = newLanguage;
-        this.#databaseRecord.save().catch((e: Error) => { console.error("Error saving language for chat ", this.#databaseRecord.chatId, e) });
     }
 
     async delete() {
