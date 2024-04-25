6ba5eb49d8fa7277a3e1c964f034a443728cf276
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-04-25 01:21:40 -0400

    also initialize some sample Chats in setup

--- server/database/MessageDatabase.test.ts
+++ server/database/MessageDatabase.test.ts
@@ -7,6 +7,11 @@ import UserDatabase from "./UserDatabase";
 beforeAll(async () => {
     await Database.initialize()
     await UserDatabase.createUser("some-messagedb-test-user-id", "John", "Doe", "English", ["Korean"]);
+
+    await ChatDatabase.createChat("sample-chat-1-id", "some-messagedb-test-user-id", "dummy chat 1 eng", "English");
+    await ChatDatabase.createChat("sample-chat-2-id", "some-messagedb-test-user-id", "dummy chat 2 eng", "English");
+    await ChatDatabase.createChat("sample-chat-3-id", "some-messagedb-test-user-id", "dummy chat 3 kor", "Korean");
+    await ChatDatabase.createChat("sample-chat-4-id", "some-messagedb-test-user-id", "dummy chat 4 eng", "English");
 });
 
 afterAll(async () => {
