e2725b779b44e01fcd1030f42be5016bd12cb93a
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-05-04 01:51:33 -0400

    convert chat-delete from id to class

--- client/src/pages/ChatRoom.tsx
+++ client/src/pages/ChatRoom.tsx
@@ -63,7 +63,7 @@ export default function ChatRoom() {
         }
       });
   }, []);
-  
+
   // Performing speech-to-text
   function handleDictation() {
     if (speechRecognition.current === null) {
@@ -80,10 +80,10 @@ export default function ChatRoom() {
     setMicActive(true);
     speechRecognition.current.start();
   }
-  
+
   async function handleFormSubmit(event) {
     event.preventDefault();
-    if(inputMessage.trim().length > 0) {
+    if (inputMessage.trim().length > 0) {
       sentMessageBuffer.current = inputMessage;
       setInputMessage("");
       setJustSent(true);
@@ -105,7 +105,7 @@ export default function ChatRoom() {
     }
     setConversations([...conversations, conversation]);
   }
-  
+
   function handleLogout() {
     window.location.href = "/logout";
   }
@@ -119,33 +119,32 @@ export default function ChatRoom() {
     document.getElementById("chatroom")!.style.visibility = "visible";
     document.getElementById("settings")!.style.visibility = "hidden";
   }
-  
+
   // Generate the conversation list
   function getConversationList() {
     return conversations.map((conversation, index) =>
       <div className="chat">
-        <button className="chat-overview" 
-                onClick={() => setSelectedConversation(index)}
-                id={`${index === selectedConversation ? "active-chat" : ""}`}>
+        <button className="chat-overview"
+          onClick={() => setSelectedConversation(index)}
+          id={`${index === selectedConversation ? "active-chat" : ""}`}>
           <p className="chat-nickname">{conversation.nickname}</p>
-          <button id="chat-delete"
-                  onClick={async (e) => 
-                    {
-                      e.stopPropagation();
-                      if(window.confirm("Do you want to delete chat " + conversation.nickname + "?")) {
-                      try {
-                        await conversation.delete();
-                        if(selectedConversation > 0) {
-                          setSelectedConversation(selectedConversation - 1);
-                        }
-                        conversations.splice(index, 1);
-                        setConversations([...conversations]);
-                      } catch (error) {
-                        console.error(error);
-                      }
-                    }
-                  }}>
-                  <FontAwesomeIcon icon={faX} />
+          <button className="chat-delete"
+            onClick={async (e) => {
+              e.stopPropagation();
+              if (window.confirm("Do you want to delete chat " + conversation.nickname + "?")) {
+                try {
+                  await conversation.delete();
+                  if (selectedConversation > 0) {
+                    setSelectedConversation(selectedConversation - 1);
+                  }
+                  conversations.splice(index, 1);
+                  setConversations([...conversations]);
+                } catch (error) {
+                  console.error(error);
+                }
+              }
+            }}>
+            <FontAwesomeIcon icon={faX} />
           </button>
         </button>
       </div>
@@ -168,7 +167,7 @@ export default function ChatRoom() {
       messageHistory.unshift(
         <Message
           key={"impossible-id"}
-          message={{role: "user", content: sentMessageBuffer.current, starred: false, timestamp: Date.now()}}
+          message={{ role: "user", content: sentMessageBuffer.current, starred: false, timestamp: Date.now() }}
           selectedLanguage={selectedLanguage}
         />
       )
@@ -177,81 +176,81 @@ export default function ChatRoom() {
   }
 
   return (
-  <>
-  {/** Settings */}
-    <div id="settings">
-      <div id="settings-header">
-        <h2>Settings</h2>
-        <FontAwesomeIcon id="exit-settings" icon={faX} onClick={closeSettings}></FontAwesomeIcon>
-      </div>
-      <div className="setting-wrapper">
-        <p>Toggle automatic text-to-speech: </p>
-        <label className="switch">
-          <input id="setting-tts" type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
-      <span className="setting-description">If on, Linguabot will always read out loud its texts!</span>
-      <div className="setting-wrapper">
-        <p>Toggle automatic speech-to-text: </p>
-        <label className="switch">
-          <input id="setting-stt" type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
-      <span className="setting-description">If on, your mic will always pick up what you say when it's your turn to send a message!</span>
-    </div>
-
-  {/** Side panel for saved chats and creating a new chat */}
-  <div id="chatroom">
-    <div id="sidebar">
-      <button id="sidebar-addchat" onClick={handleCreateNewChat}><FontAwesomeIcon icon={faPlus} id="sidebar-plus"/> Create New Chat</button>
-      <div id="sidebar-chat-list">
-        {getConversationList()}
-      </div>
-      <div id="sidebar-nav">
-        <Link className="sidebar-nav-link" to="/"> 
-          <FontAwesomeIcon icon={faHouse} /> 
-          <span className="tooltiptext" id="toolkit-home">Return Home</span>
-        </Link>
-        <Link className="sidebar-nav-link" to="/Notes"> 
-          <FontAwesomeIcon icon={faNoteSticky} /> 
-          <span className="tooltiptext" id="toolkit-notes">Saved Messages</span>
-        </Link>
-        <div className="sidebar-nav-link" onClick={openSettings}> 
-          <FontAwesomeIcon icon={faGear} /> 
-          <span className="tooltiptext" id="toolkit-settings">Settings</span>
+    <>
+      {/** Settings */}
+      <div id="settings">
+        <div id="settings-header">
+          <h2>Settings</h2>
+          <FontAwesomeIcon id="exit-settings" icon={faX} onClick={closeSettings}></FontAwesomeIcon>
         </div>
-        <div className="sidebar-nav-link" onClick={handleLogout}> 
-          <FontAwesomeIcon icon={faRightFromBracket} /> 
-          <span className="tooltiptext" id="toolkit-logout">Log Out</span>
+        <div className="setting-wrapper">
+          <p>Toggle automatic text-to-speech: </p>
+          <label className="switch">
+            <input id="setting-tts" type="checkbox" />
+            <span className="slider round"></span>
+          </label>
         </div>
+        <span className="setting-description">If on, Linguabot will always read out loud its texts!</span>
+        <div className="setting-wrapper">
+          <p>Toggle automatic speech-to-text: </p>
+          <label className="switch">
+            <input id="setting-stt" type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <span className="setting-description">If on, your mic will always pick up what you say when it's your turn to send a message!</span>
       </div>
-    </div>
 
-    <div id="chat-box">
-      {/** Text messages */}
-      <div id="chat-messages">
-        {getMessageHistory()}
-      </div>
+      {/** Side panel for saved chats and creating a new chat */}
+      <div id="chatroom">
+        <div id="sidebar">
+          <button id="sidebar-addchat" onClick={handleCreateNewChat}><FontAwesomeIcon icon={faPlus} id="sidebar-plus" /> Create New Chat</button>
+          <div id="sidebar-chat-list">
+            {getConversationList()}
+          </div>
+          <div id="sidebar-nav">
+            <Link className="sidebar-nav-link" to="/">
+              <FontAwesomeIcon icon={faHouse} />
+              <span className="tooltiptext" id="toolkit-home">Return Home</span>
+            </Link>
+            <Link className="sidebar-nav-link" to="/Notes">
+              <FontAwesomeIcon icon={faNoteSticky} />
+              <span className="tooltiptext" id="toolkit-notes">Saved Messages</span>
+            </Link>
+            <div className="sidebar-nav-link" onClick={openSettings}>
+              <FontAwesomeIcon icon={faGear} />
+              <span className="tooltiptext" id="toolkit-settings">Settings</span>
+            </div>
+            <div className="sidebar-nav-link" onClick={handleLogout}>
+              <FontAwesomeIcon icon={faRightFromBracket} />
+              <span className="tooltiptext" id="toolkit-logout">Log Out</span>
+            </div>
+          </div>
+        </div>
 
-      {/** Input and send message box */}
-      <div id="chat-text-wrapper">
-        <form id="chat-text" onSubmit={(event) => handleFormSubmit(event)}>
-          <textarea
+        <div id="chat-box">
+          {/** Text messages */}
+          <div id="chat-messages">
+            {getMessageHistory()}
+          </div>
+
+          {/** Input and send message box */}
+          <div id="chat-text-wrapper">
+            <form id="chat-text" onSubmit={(event) => handleFormSubmit(event)}>
+              <textarea
                 id="user-text-type"
                 name="user-text-type"
                 required-minlength="1"
                 placeholder={micActive ? "Say something..." : "Type something..."}
                 value={inputMessage}
                 onChange={(event) => setInputMessage(event.target.value)}>
-          </textarea>
-          <button type="button" title="Speech to Text" id="speech-to-text"><FontAwesomeIcon icon={faMicrophone} id={micActive ? "speech-to-text-icon-active" : "speech-to-text-icon"} onClick={handleDictation}/></button>
-          <button title="Send Text" id="user-text-send"><img id="user-text-send-icon" src="https://img.icons8.com/ios-glyphs/90/paper-plane.png" alt="paper-plane"/></button>
-        </form>
+              </textarea>
+              <button type="button" title="Speech to Text" id="speech-to-text"><FontAwesomeIcon icon={faMicrophone} id={micActive ? "speech-to-text-icon-active" : "speech-to-text-icon"} onClick={handleDictation} /></button>
+              <button title="Send Text" id="user-text-send"><img id="user-text-send-icon" src="https://img.icons8.com/ios-glyphs/90/paper-plane.png" alt="paper-plane" /></button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  </>
+    </>
   );
 }
